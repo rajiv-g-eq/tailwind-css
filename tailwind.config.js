1fcbcf9ab45355444be7ce4f392807bdcb297eaf
Rajiv M. Gohel <rajiv.gohel@equestsolutions.net>
2025-03-20 19:31:44 +0530

    Pro Bundle Benefits - continue section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["*"],
   theme: {
+    screens: {
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+  
+      'lg': '992px',
+      // => @media (min-width: 992px) { ... }
+  
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   plugins: [],
